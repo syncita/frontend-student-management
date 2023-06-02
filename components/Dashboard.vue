41304100da7a2c1a3fe7e0369df2a4f6721b4f44
no <112616557+syncita@users.noreply.github.com>
2023-06-02 15:44:58 +0545

    next

---
+++ components/Dashboard.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="dashboard">
+      <div class="sidebar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Reports</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </div>
+      <div class="main-content">
+        <h1>{{ pageTitle }}</h1>
+        <p>Welcome to the {{ pageTitle }}!</p>
+        <button @click="changePageTitle">Change Page Title</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        pageTitle: 'Dashboard'
+      }
+    },
+    methods: {
+      changePageTitle() {
+        this.pageTitle = 'New Page Title';
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .dashboard {
+    display: flex;
+  }
+  
+  .sidebar {
+    width: 200px;
+    background: #f0f0f0;
+    padding: 20px;
+  }
+  
+  .sidebar ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  .sidebar li {
+    margin-bottom: 10px;
+  }
+  
+  .sidebar a {
+    text-decoration: none;
+    color: #333;
+  }
+  
+  .main-content {
+    flex: 1;
+    padding: 20px;
+  }
+  </style>
+  
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,16 @@ app:{
     // nuxt.config.js
      head: {
       link: [
-        { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' }
+        { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' },
+        { rel: 'stylesheet', href: 'https://unpkg.com/element-ui/lib/theme-chalk/index.css'}
       ],
       script: [
         { src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js' },
         { src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js' },
-        { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js' }
+        { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js' },
+        {
+           src: 'https://unpkg.com/element-ui/lib/index.js'
+        }
       ]
     },
     // Other Nuxt.js configuration options
